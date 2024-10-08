aa8480c482b942d1c2f2c06028660c53dd60640e
DarkNBolt <mgrombiz@gmail.com>
2024-10-08 09:36:42 +0200

    Added a very important feature!

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1,2));
         Subtractor subtractor = new Subtractor();
-
+        TrustMeBro bro = new TrustTrust();
         System.out.println(subtractor.subtract(6,3));
     }
 }
